6f1d18ced2b42262b8dd43abb568d6e3dbdfd626
ccastano46 <castanocamilo522@gmail.com>
2024-02-26 20:22:03 -0500

    :( Lo siento

--- serving-web-content/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ serving-web-content/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,8 +1,12 @@
 package com.example.servingwebcontent;
 import org.springframework.stereotype.Controller;
+
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+/**
 @Controller
 public class GreetingController {
     @GetMapping("/greeting")
@@ -11,3 +15,4 @@ public class GreetingController {
 		return "greeting";
 	}
 }
+*/
